a812d67368cdaa2006bbafbabef6bce7dad433ee
iamalper <29778028+iamalper@users.noreply.github.com>
2023-12-17 19:15:38 +0300

    feat: :lipstick: show alert times

--- lib/screens/alerts_page.dart
+++ lib/screens/alerts_page.dart
@@ -21,33 +21,35 @@ class AlertsPage extends StatelessWidget {
           "$cityName için hava durumu uyarıları",
           style: const MyTextStyles.big(),
         ),
-        Expanded(
-          child: Padding(
-            padding: const EdgeInsets.all(12.0),
-            child: alerts.isEmpty
-                ? Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Image.asset(
-                        "assets/screen_icons/safe-weather.png",
-                        height: MediaQuery.sizeOf(context).height / 3,
-                        width: MediaQuery.sizeOf(context).width / 3,
-                        fit: BoxFit.contain,
-                      ),
-                      const Text(
-                        "Herhangi bir uyarı yok",
-                        style: MyTextStyles.medium(),
-                      ),
-                    ],
-                  )
-                : ListView.separated(
+        alerts.isEmpty
+            ? Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Image.asset(
+                    "assets/screen_icons/safe-weather.png",
+                    height: MediaQuery.sizeOf(context).height / 3,
+                    width: MediaQuery.sizeOf(context).width / 3,
+                    fit: BoxFit.contain,
+                  ),
+                  const Text(
+                    "Herhangi bir uyarı yok",
+                    style: MyTextStyles.medium(),
+                  ),
+                ],
+              )
+            : Expanded(
+                //Material widget in hereis a workaround for
+                //https://github.com/flutter/flutter/issues/86584#issuecomment-916407888
+                child: Material(
+                  child: ListView.separated(
                     itemBuilder: (context, index) => alerts[index].listTile,
                     separatorBuilder: (context, index) => const Divider(
-                          height: 5,
-                        ),
-                    itemCount: alerts.length),
-          ),
-        ),
+                      height: 5,
+                    ),
+                    itemCount: alerts.length,
+                  ),
+                ),
+              ),
       ],
     );
   }
--- lib/view_models/alert_view.dart
+++ lib/view_models/alert_view.dart
@@ -1,16 +1,28 @@
 import 'package:flutter/material.dart';
 import 'package:meteo_uyari/models/alert.dart';
 import 'package:meteo_uyari/themes.dart';
+import 'package:intl/intl.dart' show NumberFormat;
 
+final _timeIntFormat = NumberFormat("00");
 ListTile alertListTile(Alert alert) => ListTile(
-    leading: Image.asset("assets/alert_icons/${alert.hadise.name}.png"),
-    title: Text(
-      alert.hadise.baslik,
-      style: const MyTextStyles.medium(),
-    ),
-    subtitle: Text(
-      alert.description,
-      style: const MyTextStyles.small(),
-    ),
-    tileColor: alert.color,
-    shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(20)));
+      leading: Image.asset("assets/alert_icons/${alert.hadise.name}.png"),
+      title: Text(
+        alert.hadise.baslik,
+        style: const MyTextStyles.medium(),
+      ),
+      subtitle: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          Text(
+            alert.description,
+            style: const MyTextStyles.small(),
+          ),
+          Text(
+            "Başlangıç: ${_timeIntFormat.format(alert.beginTime.hour)}:${_timeIntFormat.format(alert.beginTime.minute)} Bitiş: ${_timeIntFormat.format(alert.endTime.hour)}:${_timeIntFormat.format(alert.endTime.minute)}",
+            style: const MyTextStyles.small().copyWith(color: Colors.blue),
+          )
+        ],
+      ),
+      tileColor: alert.color,
+      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(20)),
+    );
